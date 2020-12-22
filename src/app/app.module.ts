996441f4ad0bfa2835fe65685b77f78194b51ef7
Muffin <marvin.dick96@web.de>
2020-12-22 22:32:21 +0100

    loading spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatCardModule} from '@angular/material/card';
-import {MatListModule} from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { ReadListComponent } from './components/read-list/read-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ReadListComponent
-  ],
+  declarations: [AppComponent, ReadListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     MatCardModule,
-    MatListModule
+    MatListModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
